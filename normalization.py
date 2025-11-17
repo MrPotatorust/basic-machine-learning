388e103d4f84b2963a78708f2ace38a2d743fe5e
Martin Jaros <martinj@webgaleria.sk>
2025-11-17 17:20:04 +0100

    started working on normalization for dataset

---
+++ normalization.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#total_bill = float => float
+#tip = float => float
+#sex = str => int (Male: 0, Female: 1)
+#smoker = str => int (No: 0, Yes: 1)
+#day = str => int (Thur: 0, Fri: 1, Sat: 2, Sun: 3)
+#time = str => int (Lunch: 0, Dinner: 1)
+#size = int => int
+
+
+def main():
+    df = pd.read_csv('./dataset/tips.csv')
+
+    df.to_csv('./dataset/normalized_tips.csv', index=False)
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
