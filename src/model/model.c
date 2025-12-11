df6db80548a29b467fa2324f38bb2977f21c1011
Martin Jaros <martinj@webgaleria.sk>
2025-12-11 23:52:55 +0100

    feat: added calculateWeightSlope and calculateBiasSlope functions

--- src/model/model.c
+++ src/model/model.c
@@ -1,7 +1,8 @@
 #include "includes.h"
 #include "model.h"
 
-struct Model model_Constructor(){
+struct Model model_Constructor()
+{
     struct Model self;
 
     self.weights = NULL;
@@ -20,6 +21,8 @@ struct Model model_Constructor(){
     self.train = train;
     self.loadData = loadData;
     self.setTrainingConfig = setTrainingConfig;
+    self.calculateWeightSlope = calculateWeightSlope;
+    self.calculateBiasSlope = calculateBiasSlope;
     self.calculateLoss = calculateLoss;
     self.predict = predict;
     self.resetWeights = resetWeights;
@@ -27,7 +30,8 @@ struct Model model_Constructor(){
     return self;
 }
 
-void loadData(struct Model *self, CSV *csv, char *labelName, char **featureNames, int featureCount){
+void loadData(struct Model *self, CSV *csv, char *labelName, char **featureNames, int featureCount)
+{
     self->data = csv;
     self->labelName = labelName;
     self->featureNames = featureNames;
@@ -36,45 +40,89 @@ void loadData(struct Model *self, CSV *csv, char *labelName, char **featureNames
     self->resetWeights(self);
 }
 
-void setTrainingConfig(struct Model *self, int epochs, float learningRate, float batchSize){
+void setTrainingConfig(struct Model *self, int epochs, float learningRate, float batchSize)
+{
     self->epochs = epochs;
     self->learningRate = learningRate;
     self->batchSize = batchSize;
 }
 
-void train(struct Model *self){
+void train(struct Model *self)
+{
 
     self->loss = self->calculateLoss(self);
-    
-    
+
+    float *newWeights = malloc(self->featureCount * sizeof(float));
+
+    self->calculateWeightSlope(self, 0);
+    self->calculateBiasSlope(self);
 
     printf("  ====================================================== \n");
     printf("|| Weight: %f || Bias: %f || Loss: %f || \n", self->weights[0], self->bias, self->loss);
-
 };
 
-float calculateLoss(struct Model *self){
+float calculateWeightSlope(struct Model *self, int weightIndex)
+{
+
+    float aggregate = 0;
+
+    for (int curRow = 0; curRow < self->data->rowCount; curRow++)
+    {
+        float feature = getData(self->data, curRow, weightIndex);
+        float label = getData(self->data, curRow, self->featureCount - 1);
+        Row *startFeaturePointer = &self->data->rows[curRow * self->data->collumnCount];
+        float prediction = self->predict(self, startFeaturePointer);
+
+        aggregate += (prediction - label) * (2 * feature);
+    }
+
+    float slope = aggregate / self->data->rowCount;
+
+    return slope;
+}
+
+float calculateBiasSlope(struct Model *self)
+{
+    float aggregate = 0;
+
+    for (int curRow = 0; curRow < self->data->rowCount; curRow++)
+    {
+        float label = getData(self->data, curRow, self->featureCount - 1);
+        Row *startFeaturePointer = &self->data->rows[curRow * self->data->collumnCount];
+        float prediction = self->predict(self, startFeaturePointer);
+
+        aggregate += (prediction - label) * 2;
+    }
+
+    float slope = aggregate / self->data->rowCount;
+
+    return slope;
+}
+
+float calculateLoss(struct Model *self)
+{
     int exampleCount = self->data->rowCount;
 
     float lossAggregate = 0;
 
-    
-    for(int curCol = 0; curCol < exampleCount; curCol++){
-        Row *startFeaturePointer = &self->data->rows[curCol * self->data->collumnCount];
-        float label = getData(self->data, curCol, self->featureCount);
+    for (int curRow = 0; curRow < exampleCount; curRow++)
+    {
+        Row *startFeaturePointer = &self->data->rows[curRow * self->data->collumnCount];
+        float label = getData(self->data, curRow, self->featureCount);
 
         float predictionVal = self->predict(self, startFeaturePointer);
         lossAggregate += pow(label - predictionVal, 2);
     }
 
-
     return lossAggregate / exampleCount;
 }
 
-float predict(struct Model *self, float features[]){
+float predict(struct Model *self, float features[])
+{
     float prediction = 0;
-    
-    for(int curCol = 0; curCol < self->featureCount; curCol++){
+
+    for (int curCol = 0; curCol < self->featureCount; curCol++)
+    {
         prediction += features[curCol] * self->weights[curCol];
     }
 
@@ -83,28 +131,27 @@ float predict(struct Model *self, float features[]){
     return prediction;
 }
 
+void resetWeights(struct Model *self)
+{
 
-void resetWeights(struct Model *self){
-
-    if(!self->featureCount){
+    if (!self->featureCount)
+    {
         perror("No features are set");
         return;
     }
 
     float *weights = malloc(self->featureCount * sizeof(float));
 
-    if(!weights){
+    if (!weights)
+    {
         perror("Could not initialize memory for weights");
         return;
     }
 
-    for(int i = 0; i < self->featureCount; i++){
+    for (int i = 0; i < self->featureCount; i++)
+    {
         weights[i] = 0.0;
     }
 
     self->weights = weights;
-}
-
-float calculateWeightSlope(struct Model *self){
-
 }
--- src/model/model.h
+++ src/model/model.h
@@ -3,10 +3,10 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-struct Model {
+struct Model
+{
     float *weights;
     float bias;
-
 
     char *labelName;
     char **featureNames;
@@ -19,10 +19,11 @@ struct Model {
     float batchSize;
     int epochs;
 
-
     void (*train)(struct Model *self);
     void (*loadData)(struct Model *self, CSV *csv, char *labelName, char **featureNames, int featureCount);
     void (*setTrainingConfig)(struct Model *self, int epochs, float learningRate, float batchSize);
+    float (*calculateWeightSlope)(struct Model *self, int weightIndex);
+    float (*calculateBiasSlope)(struct Model *self);
     float (*calculateLoss)(struct Model *self);
     float (*predict)(struct Model *self, float features[]);
     void (*resetWeights)(struct Model *self);
@@ -31,6 +32,8 @@ struct Model {
 void loadData(struct Model *self, CSV *csv, char *labelName, char **featureNames, int featureCount);
 void setTrainingConfig(struct Model *self, int epochs, float learningRate, float batchSize);
 void train(struct Model *self);
+float calculateWeightSlope(struct Model *self, int weightIndex);
+float calculateBiasSlope(struct Model *self);
 //? Right now the loss is calculated on all examples and calculates MSE
 float calculateLoss(struct Model *self);
 //! Here you have to pass the same features as the model was trained with
@@ -38,6 +41,5 @@ float predict(struct Model *self, float *features);
 void resetWeights(struct Model *self);
 
 struct Model model_Constructor();
-
 
 #endif
