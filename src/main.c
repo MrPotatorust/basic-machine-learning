82b69a98a99e752a32a17d7f173854c26128464f
Martin Jaros <martinj@webgaleria.sk>
2025-12-12 15:35:15 +0100

    feat: changed learningRate

--- src/main.c
+++ src/main.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 
     model.loadData(&model, csv, labelName, featureNames, featureCount);
 
-    int epochs = 12;
-    float learningRate = 0.1;
+    int epochs = 0;
+    float learningRate = 0.001;
     int batchSize = 20;
 
     model.setTrainingConfig(&model, epochs, learningRate, batchSize);
 
     model.train(&model);
-    // float features[] ={2.1};
+    // float features[] ={14.83, 1, 0, 3, 1, 2};
     // float prediction = model.predict(&model, features);
     // printf("Prediction %f \n", prediction);
 
